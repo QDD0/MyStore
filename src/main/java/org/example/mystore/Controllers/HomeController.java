2a62d536627116db17f8839ac5eb4df16ccfc3b7
Den <dend51108@gmail.com>
2025-07-17 23:15:23 +0300

    + women.html

--- src/main/java/org/example/mystore/Controllers/HomeController.java
+++ src/main/java/org/example/mystore/Controllers/HomeController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping({"/", "/men"})
     public String home() {
+        return "men";
+    }
+
+    @RequestMapping("/women")
+    public String women() {
+        return "women";
     }
 }
