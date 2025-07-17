6a78c93433ccf7449c99eff5cde49a39e0a77476
Den <dend51108@gmail.com>
2025-07-17 14:17:43 +0300

    first commit

---
+++ src/test/java/org/example/mystore/MyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
